d0bec53484f124582ae42c68da93e46b999644df
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-21 21:32:11 +0100

    feat: Added UserRepository and corresponding functions

--- src/main/java/com/booking/project/repository/inteface/IUserRepository.java
+++ src/main/java/com/booking/project/repository/inteface/IUserRepository.java
@@ -1,2 +1,18 @@
-package com.booking.project.repository.inteface;public class IUserRepository {
+package com.booking.project.repository.inteface;
+
+import com.booking.project.model.User;
+
+import java.util.Collection;
+
+public interface IUserRepository {
+
+    Collection<User> findAll();
+
+    User find(Long id);
+
+    User create(User greeting) throws Exception;
+
+    User update(User greeting) throws Exception;
+
+    void delete(Long id);
 }
