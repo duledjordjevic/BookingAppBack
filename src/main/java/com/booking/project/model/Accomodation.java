3cf028b2a19fd88ed9df34c9b9d3f478e3ced727
Maric002 Nikola Maric <n.maric1912@gmail.com>
2023-11-21 22:36:21 +0100

    Add attributes in accomodation model

--- src/main/java/com/booking/project/model/Accomodation.java
+++ src/main/java/com/booking/project/model/Accomodation.java
@@ -1,12 +1,23 @@
 package com.booking.project.model;
 
+import com.booking.project.model.enums.AccomodationType;
+import com.booking.project.model.enums.Amenities;
+import com.booking.project.model.enums.CancellationPolicy;
+import com.booking.project.model.enums.ReservationMethod;
+
+import java.util.List;
+
 public class Accomodation {
     private String title;
     private String description;
     private Address location;
+    private List<Amenities> amenities;
     private Long minGuests;
     private Long maxGuests;
+    private AccomodationType type;
+    private CancellationPolicy cancellationPolicy;
     private boolean isAvailableForReservation;
     private double raiting;
+    private ReservationMethod reservationMethod;
 
 }
