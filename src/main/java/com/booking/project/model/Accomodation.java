5de59c02df42579cbbe96bbc96a43f23e05234f1
Maric002 Nikola Maric <n.maric1912@gmail.com>
2023-11-21 22:22:47 +0100

    Add Accomodation model

---
+++ src/main/java/com/booking/project/model/Accomodation.java
@@ -0,0 +1,12 @@
+package com.booking.project.model;
+
+public class Accomodation {
+    private String title;
+    private String description;
+    private Address location;
+    private Long minGuests;
+    private Long maxGuests;
+    private boolean isAvailableForReservation;
+    private double raiting;
+
+}
