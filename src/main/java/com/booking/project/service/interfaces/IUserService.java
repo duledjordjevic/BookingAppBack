3df0c2506ad2d2e13b8f67ff7b00b4a72c830e57
unknown <djordjevic.sv1.2021@uns.ac.rs>
2023-11-21 19:18:20 +0100

    Add UserService

---
+++ src/main/java/com/booking/project/service/interfaces/IUserService.java
@@ -0,0 +1,18 @@
+package com.booking.project.service.interfaces;
+
+import com.booking.project.model.User;
+
+import java.util.Collection;
+
+public interface IUserService {
+
+    Collection<User> findAll();
+
+    User findOne(Long id);
+
+    User create(User greeting) throws Exception;
+
+    User update(User greeting) throws Exception;
+
+    void delete(Long id);
+}
