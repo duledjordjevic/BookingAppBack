1dc82cead8e22cae9dfa61cc9594c759bb9e22e0
unknown <djordjevic.sv1.2021@uns.ac.rs>
2023-11-22 22:42:58 +0100

    Add GuestController

---
+++ src/main/java/com/booking/project/controller/GuestController.java
@@ -0,0 +1,65 @@
+package com.booking.project.controller;
+
+import com.booking.project.model.Guest;
+import com.booking.project.model.User;
+import com.booking.project.service.interfaces.IGuestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/guests")
+public class GuestController {
+
+    @Autowired
+    private IGuestService guestService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<Guest>> getGuests(){
+        Collection<Guest> guests = guestService.findAll();
+        return new ResponseEntity<Collection<Guest>>(guests, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Optional<Guest>> getGuest(@PathVariable("id") Long id){
+        Optional<Guest> guest = guestService.find(id);
+        if(guest.isEmpty()){
+            return new ResponseEntity<Optional<Guest>>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Optional<Guest>>(guest, HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Guest> createGuest(@RequestBody Guest guest) throws Exception {
+        Guest savedGuest = guestService.create(guest);
+        return new ResponseEntity<Guest>(savedGuest, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Guest> updateGuest(@RequestBody Guest guest, @PathVariable Long id) throws Exception{
+        Optional<Guest> guestForUpdate = guestService.find(id);
+
+        if (guestForUpdate.isEmpty()) {
+            return new ResponseEntity<Guest>(HttpStatus.BAD_REQUEST);
+        }
+
+        guestForUpdate.get().setName(guest.getName());
+        guestForUpdate.get().setLastName(guest.getLastName());
+        guestForUpdate.get().setUser(guest.getUser());
+        guestForUpdate.get().setAddress(guest.getAddress());
+
+        return new ResponseEntity<Guest>(guestService.update(guestForUpdate.get()), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Guest> deleteGuest(@PathVariable("id") Long id){
+        guestService.delete(id);
+        return new ResponseEntity<Guest>(HttpStatus.NO_CONTENT);
+    }
+
+}
