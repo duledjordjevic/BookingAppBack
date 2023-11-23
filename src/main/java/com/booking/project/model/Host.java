799f95feef5f58d02e6d0709044809530485b903
unknown <djordjevic.sv1.2021@uns.ac.rs>
2023-11-23 21:59:24 +0100

    Change constraints and relations for entities

--- src/main/java/com/booking/project/model/Host.java
+++ src/main/java/com/booking/project/model/Host.java
@@ -1,9 +1,16 @@
 package com.booking.project.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
+import java.util.List;
 
 @Entity
 @Table(name = "hosts")
 public class Host extends Person{
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Accommodation> accommodations;
 }
