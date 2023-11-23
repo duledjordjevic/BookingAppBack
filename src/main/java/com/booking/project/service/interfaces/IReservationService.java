c5e4a7ca908d5fd4c09b8c9a6f78df8e241d2221
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-23 23:27:07 +0100

    feat: Added Reservation service interface

--- src/main/java/com/booking/project/service/interfaces/IReservationService.java
+++ src/main/java/com/booking/project/service/interfaces/IReservationService.java
@@ -1,2 +1,16 @@
-package com.booking.project.service.interfaces;public interface IReservationService {
+package com.booking.project.service.interfaces;
+
+import com.booking.project.model.Reservation;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface IReservationService {
+    Collection<Reservation> findAll();
+
+    Optional<Reservation> findById(Long id);
+
+    Reservation save(Reservation reservation) throws Exception;
+
+    void deleteById(Long id);
 }
