f703326f00714e9feece450f9e1c581274b8390b
Maric002 Nikola Maric <n.maric1912@gmail.com>
2023-11-23 13:38:01 +0100

    Change accomodation repo

--- src/main/java/com/booking/project/repository/inteface/IAccomodationRepository.java
+++ src/main/java/com/booking/project/repository/inteface/IAccomodationRepository.java
@@ -1,17 +1,10 @@
 package com.booking.project.repository.inteface;
 
 import com.booking.project.model.Accomodation;
+import com.booking.project.model.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 
-public interface IAccomodationRepository {
-    Collection<Accomodation> findAll();
-
-    Accomodation find(Long id);
-
-    Accomodation create(Accomodation accomodation) throws Exception;
-
-    Accomodation update(Accomodation accomodation) throws Exception;
-
-    void delete(Long id);
+public interface IAccomodationRepository extends JpaRepository<Accomodation, Long> {
 }
