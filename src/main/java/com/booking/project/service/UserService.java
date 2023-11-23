7c13da2d67a2f32f9a2e8bc92770abb2eb9ff98b
Maric002 Nikola Maric <n.maric1912@gmail.com>
2023-11-23 20:59:05 +0100

    Change UserService methods names

--- src/main/java/com/booking/project/service/UserService.java
+++ src/main/java/com/booking/project/service/UserService.java
@@ -1,43 +1,35 @@
 package com.booking.project.service;
 
 import com.booking.project.model.User;
-import com.booking.project.repository.UserRepository;
 import com.booking.project.repository.inteface.IUserRepository;
 import com.booking.project.service.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements IUserService {
-
     @Autowired
-    private IUserRepository userRepository;
-
+    private IUserRepository repository;
     @Override
     public Collection<User> findAll() {
-        return userRepository.findAll();
+        return repository.findAll();
     }
 
     @Override
-    public User find(Long id) {
-        return userRepository.find(id);
+    public Optional<User> findById(Long id) {
+        return repository.findById(id);
     }
 
     @Override
-    public User create(User user) throws Exception {
-        return userRepository.create(user);
+    public User save(User user) throws Exception {
+        return repository.save(user);
     }
 
     @Override
-    public User update(User user) throws Exception {
-        return userRepository.update(user);
-    }
-
-    @Override
-    public void delete(Long id) {
-        userRepository.delete(id);
+    public void deleteById(Long id) {
+        repository.deleteById(id);
     }
 }
