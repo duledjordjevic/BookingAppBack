36d971e6fac1b8f90c3a3ba88ff229f05318613c
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-25 18:47:01 +0100

    feat: Added service interface for CommentAboutAcc

--- src/main/java/com/booking/project/service/interfaces/ICommentAboutAccService.java
+++ src/main/java/com/booking/project/service/interfaces/ICommentAboutAccService.java
@@ -1,2 +1,17 @@
-package com.booking.project.service.interfaces;public interface ICommentAboutAccService {
+package com.booking.project.service.interfaces;
+
+import com.booking.project.model.CommentAboutAcc;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface ICommentAboutAccService {
+
+    Collection<CommentAboutAcc> findAll();
+
+    Optional<CommentAboutAcc> findById(Long id);
+
+    CommentAboutAcc save(CommentAboutAcc commentAboutAcc) throws Exception;
+
+    void deleteById(Long id);
 }
