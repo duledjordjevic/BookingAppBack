949dec4477c95e0a44636e8f5e733398ba279abc
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-25 18:16:40 +0100

    feat: Added repo for NotificationForHost

--- src/main/java/com/booking/project/repository/inteface/INotificationForHost.java
+++ src/main/java/com/booking/project/repository/inteface/INotificationForHost.java
@@ -1,2 +1,8 @@
-package com.booking.project.repository.inteface;public interface INotificationForHost {
+package com.booking.project.repository.inteface;
+
+
+import com.booking.project.model.NotificationForHost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface INotificationForHost extends JpaRepository<NotificationForHost,Long> {
 }
