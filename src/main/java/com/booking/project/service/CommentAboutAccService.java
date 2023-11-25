29704c27970b923704c10e4dc3e64d5a5a356e22
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-25 23:00:15 +0100

    feat: Added service for CommentAboutAcc

--- src/main/java/com/booking/project/service/CommentAboutAccService.java
+++ src/main/java/com/booking/project/service/CommentAboutAccService.java
@@ -1,2 +1,36 @@
-package com.booking.project.service.interfaces;public class CommentAboutAccService {
+package com.booking.project.service;
+
+import com.booking.project.model.CommentAboutAcc;
+import com.booking.project.repository.inteface.ICommentAboutAccRepository;
+import com.booking.project.service.interfaces.ICommentAboutAccService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class CommentAboutAccService implements ICommentAboutAccService {
+
+    @Autowired
+    private ICommentAboutAccRepository commentAboutAccRepository;
+    @Override
+    public Collection<CommentAboutAcc> findAll() {
+        return commentAboutAccRepository.findAll();
+    }
+
+    @Override
+    public Optional<CommentAboutAcc> findById(Long id) {
+        return commentAboutAccRepository.findById(id);
+    }
+
+    @Override
+    public CommentAboutAcc save(CommentAboutAcc commentAboutAcc) throws Exception {
+        return commentAboutAccRepository.save(commentAboutAcc);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        commentAboutAccRepository.deleteById(id);
+    }
 }
