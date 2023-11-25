5377c9366369d69a7f934a625a477ff83b475919
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-25 22:59:32 +0100

    feat: Changed name for NotificationForHost repo

--- src/main/java/com/booking/project/repository/inteface/INotificationForHostRepository.java
+++ src/main/java/com/booking/project/repository/inteface/INotificationForHostRepository.java
@@ -4,5 +4,5 @@ package com.booking.project.repository.inteface;
 import com.booking.project.model.NotificationForHost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface INotificationForHost extends JpaRepository<NotificationForHost,Long> {
+public interface INotificationForHostRepository extends JpaRepository<NotificationForHost,Long> {
 }
