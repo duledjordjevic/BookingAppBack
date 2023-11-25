f2410899fa7259a2d9ef0636d0dbc855a69d3879
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-25 18:48:23 +0100

    feat: Added service interface for NotificationForGuest

--- src/main/java/com/booking/project/service/interfaces/INotificationForGuestService.java
+++ src/main/java/com/booking/project/service/interfaces/INotificationForGuestService.java
@@ -1,2 +1,18 @@
-package com.booking.project.service.interfaces;public interface INotificationForGuestService {
+package com.booking.project.service.interfaces;
+
+import com.booking.project.model.CommentAboutHost;
+import com.booking.project.model.NotificationForGuest;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface INotificationForGuestService {
+
+    Collection<NotificationForGuest> findAll();
+
+    Optional<NotificationForGuest> findById(Long id);
+
+    NotificationForGuest save(NotificationForGuest notificationForGuest) throws Exception;
+
+    void deleteById(Long id);
 }
