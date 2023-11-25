71afa043a183877182d808a2bcc451395ae45d46
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-25 23:01:07 +0100

    feat: Added service for NotificationForGuest

--- src/main/java/com/booking/project/service/NotificationForGuestService.java
+++ src/main/java/com/booking/project/service/NotificationForGuestService.java
@@ -1,2 +1,35 @@
-package com.booking.project.service;public class NotificationForGuestService {
+package com.booking.project.service;
+
+import com.booking.project.model.NotificationForGuest;
+import com.booking.project.repository.inteface.INotificationForGuestRepository;
+import com.booking.project.service.interfaces.INotificationForGuestService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class NotificationForGuestService implements INotificationForGuestService {
+
+    private INotificationForGuestRepository notificationForGuestRepository;
+
+    @Override
+    public Collection<NotificationForGuest> findAll() {
+        return notificationForGuestRepository.findAll();
+    }
+
+    @Override
+    public Optional<NotificationForGuest> findById(Long id) {
+        return notificationForGuestRepository.findById(id);
+    }
+
+    @Override
+    public NotificationForGuest save(NotificationForGuest notificationForGuest) throws Exception {
+        return notificationForGuestRepository.save(notificationForGuest);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        notificationForGuestRepository.deleteById(id);
+    }
 }
