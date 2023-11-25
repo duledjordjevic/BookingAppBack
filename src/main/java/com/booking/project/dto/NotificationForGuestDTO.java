bd67cc45f9fb1bb8c55edc662623a1042d3ffe9a
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-26 00:46:28 +0100

    feat: Added dto for NotificationForGuest

--- src/main/java/com/booking/project/dto/NotificationForGuestDTO.java
+++ src/main/java/com/booking/project/dto/NotificationForGuestDTO.java
@@ -1,2 +1,22 @@
-package com.booking.project.dto;public class NotificationForGuestDTO {
+package com.booking.project.dto;
+
+import com.booking.project.model.NotificationForGuest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationForGuestDTO {
+
+    private Long id;
+    private String description;
+
+    public NotificationForGuestDTO(NotificationForGuest notificationForGuest){
+        this.id = notificationForGuest.getId();
+        this.description = notificationForGuest.getDescription();
+    }
 }
