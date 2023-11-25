c169e461d27de50c981f9f95141d0c8227c503d5
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-26 00:45:27 +0100

    feat: Added dto for CommentAboutAcc

--- src/main/java/com/booking/project/dto/CommentAboutAccDTO.java
+++ src/main/java/com/booking/project/dto/CommentAboutAccDTO.java
@@ -1,2 +1,32 @@
-package com.booking.project.dto;public class CommentAboutAccDTO {
+package com.booking.project.dto;
+
+import com.booking.project.model.CommentAboutAcc;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentAboutAccDTO {
+
+    private Long id;
+    private int rating;
+    private boolean isReported;
+    private String content;
+    private boolean isApproved;
+    private AccommodationDTO accommodationDTO;
+    private GuestDTO guestDTO;
+
+    public CommentAboutAccDTO(CommentAboutAcc commentAboutAcc){
+        this.id = commentAboutAcc.getId();
+        this.rating = commentAboutAcc.getRating();
+        this.isReported = commentAboutAcc.isReported();
+        this.content = commentAboutAcc.getContent();
+        this.isApproved = commentAboutAcc.isApproved();
+        this.accommodationDTO = new AccommodationDTO(commentAboutAcc.getAccommodation());
+        this.guestDTO = new GuestDTO(commentAboutAcc.getGuest());
+    }
 }
