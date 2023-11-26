511d87d2fc37faac1042c90b529c8afcec6d1ec0
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-27 00:40:30 +0100

    feat: Addded dto for CreateCommentAboutHost

---
+++ src/main/java/com/booking/project/dto/CreateCommentAboutHostDTO.java
@@ -0,0 +1,17 @@
+package com.booking.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCommentAboutHostDTO {
+    private int rating;
+    private String content;
+    private Long guestId;
+    private Long hostId;
+}
