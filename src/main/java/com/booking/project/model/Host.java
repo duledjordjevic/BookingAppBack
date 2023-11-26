4f58e57c11c366737c915b88770c43704f769359
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-26 13:07:16 +0100

    feat: Added copyValue function

--- src/main/java/com/booking/project/model/Host.java
+++ src/main/java/com/booking/project/model/Host.java
@@ -1,11 +1,21 @@
 package com.booking.project.model;
 
+import com.booking.project.dto.HostDTO;
 import jakarta.persistence.*;
-
-import java.util.List;
 
 @Entity
 @Table(name = "hosts")
 public class Host extends Person{
 
+    public void copyValues(HostDTO hostDTO){
+
+        this.setId(hostDTO.getId());
+        this.setName(hostDTO.getName());
+        this.setLastName(hostDTO.getLastName());
+        this.setAddress(hostDTO.getAddress());
+        this.setPhoneNumber(hostDTO.getPhoneNumber());
+        this.setNotificationEnabled(hostDTO.isNotificationEnabled());
+        User user = new User(hostDTO.getUserCredentialsDTO());
+        this.setUser(user);
+    }
 }
