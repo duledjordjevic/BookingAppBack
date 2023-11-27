b2d7da5df2ac003fca37672061bd1d047a951cc0
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-11-27 02:44:20 +0100

    feat: Added function for filtering by acc in repo

--- src/main/java/com/booking/project/repository/inteface/ICommentAboutAccRepository.java
+++ src/main/java/com/booking/project/repository/inteface/ICommentAboutAccRepository.java
@@ -1,7 +1,14 @@
 package com.booking.project.repository.inteface;
 
+import com.booking.project.model.Accommodation;
 import com.booking.project.model.CommentAboutAcc;
+import com.booking.project.model.CommentAboutHost;
+import com.booking.project.model.Host;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.Optional;
+
 public interface ICommentAboutAccRepository extends JpaRepository<CommentAboutAcc,Long> {
+    Collection<CommentAboutAcc> findAllByAccommodation(Optional<Accommodation> accommodation);
 }
