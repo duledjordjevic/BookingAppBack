bbbc384b46b32df3846e31d2c8d12c62165c7ef9
Nikola <n.maric1912@gmail.com>
2023-12-05 20:32:19 +0100

    Add ConfirmationToken model, repo and servicee

---
+++ src/main/java/com/booking/project/service/ConfirmationTokenService.java
@@ -0,0 +1,29 @@
+package com.booking.project.service;
+
+import com.booking.project.model.ConfirmationToken;
+import com.booking.project.repository.inteface.ConfirmationTokenRepository;
+import com.booking.project.service.interfaces.IConfirmationTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ConfirmationTokenService implements IConfirmationTokenService {
+    @Autowired
+    private ConfirmationTokenRepository confirmationTokenRepository;
+    @Override
+    public void saveConfirmationToken(ConfirmationToken token) {
+        confirmationTokenRepository.save(token);
+    }
+    @Override
+    public Optional<ConfirmationToken> getToken(String token) {
+        return confirmationTokenRepository.findByToken(token);
+    }
+    @Override
+    public int setConfirmedAt(String token) {
+        return confirmationTokenRepository.updateConfirmedAt(
+                token, LocalDateTime.now());
+    }
+}
