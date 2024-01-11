4daeb5e73756440c68eb5d51e3e326dd09ec90bb
pejinovics <pejinovicstefan@gmail.com>
2024-01-11 19:00:02 +0100

    feat: add host to dto

--- src/main/java/com/booking/project/dto/AccommodationDTO.java
+++ src/main/java/com/booking/project/dto/AccommodationDTO.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 @Getter
@@ -48,6 +49,4 @@ public class AccommodationDTO {
         this.cancellationPolicy = accommodation.getCancellationPolicy();
         this.host = accommodation.getHost();
     }
-
-
 }
