1a7c28f3fc6f1dc52758740447cd7847fc3a02fb
Dusan Djordjevic <djordjevic.sv1.2021@uns.ac.rs>
2024-04-22 11:36:21 +0200

    feat: add https and cors

--- src/main/java/com/booking/project/config/WebConfig.java
+++ src/main/java/com/booking/project/config/WebConfig.java
@@ -8,6 +8,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://localhost:4201")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");    }
 }
